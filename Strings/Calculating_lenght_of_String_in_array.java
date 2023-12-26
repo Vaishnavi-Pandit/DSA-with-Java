69d7ba20b76a08b06c9dee4d41bb3cab35239281
Vaishnavi-Pandit <vaishnavipandit242@gmail.com>
2023-12-26 18:09:51 +0530

    26/12/23

---
+++ Strings/Calculating_lenght_of_String_in_array.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+public class Calculating_lenght_of_String_in_array {
+    /**
+     * @param args
+     */
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int l=0;
+        String array[]= new String[n];
+        for(int i=0;i<n;i++){
+            array[i]=sc.nextLine();
+
+        }
+         for(int i=0;i<n;i++){
+         l=l+array[i].length();
+
+        }
+        System.out.println(l);
+
+    }
+}
