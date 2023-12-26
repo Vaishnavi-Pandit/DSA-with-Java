9560c6c88c4702f66afeb95ffafc14f3d6f92f34
Vaishnavi-Pandit <vaishnavipandit242@gmail.com>
2023-12-26 16:28:16 +0530

    new

---
+++ array/Weather_array_is_in_ascending_order_or_not.java
@@ -0,0 +1,40 @@
+
+
+import java.util.Scanner;
+public class Weather_array_is_in_ascending_order_or_not {
+    /**
+     * @param array
+     * @return
+     */
+    public static int ascen (int array[]){
+        boolean flag=true;
+        for(int i=0;i<array.length-1;i++){
+            if(array[i]>array[i+1]){
+                flag=false;
+            }
+            }
+            if(flag ==false){
+                System.out.print("not in ascending order");
+
+            }
+            else{
+                System.out.print(" in ascending order"); 
+            }
+        return 0;
+    }
+/**
+ * @param args
+ */
+public static void main (String args [])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int array[]=new int[n];
+        for(int i=0;i<n;i++){
+            array[i]=sc.nextInt();
+        }
+        ascen(array);
+
+    }
+
+}
